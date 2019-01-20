b324bde170cd4078334189f2d472af5c175b1537
David Downie <daviddownie@gmail.com>
2019-01-20 18:32:03 -0500

    name input connectiong to API

--- models/user.js
+++ models/user.js
@@ -1,23 +1,24 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     UserId: {
-     type: DataTypes.INTEGER,
-     allowNull : false,
-     primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
     },
     Password: {
       type: DataTypes.STRING,
-      allowNull :false,
-      defaultValue:false
+      allowNull: false,
+      defaultValue: false
     }
-    });
+  });
 
-    User.associate = function(models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
-      User.hasMany(models.Bill, {
-        onDelete: "cascade"
-      });
-    };
+  User.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Bill, {
+      onDelete: "cascade"
+    });
+  };
   return User;
 };
