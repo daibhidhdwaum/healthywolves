e790697364f8c4cd0175809cde490bfe135add62
Saryn Caister <thegreenmajority@gmail.com>
2019-01-21 19:06:04 -0500

    not sure what this is honestly

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,40 @@
 // Get references to page elements
-var $newUser = $("#new-user");
-var $submitBtn = $("#submit");
+var $newUser = $("#new-user"); // new user name field
+var $newUserPass = $("#new-user-pass"); // new user password field
+var $newUserSubmit = $("#new-user-submit"); // create new user submit button
+
+var $loginUser = $("#user-login"); // user login name field
+var $loginPass = $("#user-login-pass"); // user login password field
+var $loginBtn = $("#submit"); // user login submit button
+
+
 var $existingUsers = $("#existing-users");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  createUser: function(users) {
+  createUser: function (users) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/users",
+      url: "api/users/",
       data: JSON.stringify(users)
     });
   },
-  getUsers: function() {
+  getUsers: function () {
     return $.ajax({
-      url: "api/users",
+      url: "api/users/",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  logginUser: function () {
+    return $.ajax({
+      url: "api/users/",
+      type: "GET"
+    });
+  },
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/users/" + id,
       type: "DELETE"
@@ -30,9 +43,9 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getUsers().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getUsers().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>").text(example.Password);
       // .attr("href", "/api/user/" + example.UserId);
 
@@ -62,11 +75,25 @@ refreshExamples();
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
 
-var newUser = {
-  text: $newUser.val().trim()
+var newUser = function () {
+  API.createUser({
+    userName: $newUser.val().trim(),
+    Password: $newUserPass.val().trim()
+  }).then(function () {
+    refreshExamples();
+  });
 };
 
-$newUser.val("");
+var logginUser = function () {
+  API.getUsers().then(function () {
+    // if (){
+    // password check for login goes here. I assume this will use getUsers above to check the user database for a name match, and then if that succedds, check the password against password.
+    // if the check works, it will redirect to loggedin.handlebars
+    //if it fails, it should throw an alert (or even a modal) to try again and refresh the page.
+    // });
+    refreshExamples();
+  });
+};
 
 // if (!(example.text && example.description)) {
 //   alert("You must enter an example text and description!");
@@ -81,16 +108,17 @@ $newUser.val("");
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
 
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+// var handleDeleteBtnClick = function() {
+//   var idToDelete = $(this)
+//     .parent()
+//     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
+//   API.deleteExample(idToDelete).then(function() {
+//     refreshExamples();
+//   });
+// };
 
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$existingUsers.on("click", ".delete", handleDeleteBtnClick);
+// Add event listeners to both submits and the delete buttons
+$newUserSubmit.on("click", newUser);
+$loginBtn.on("click", logginUser);
+// $deleteItem.on("click", ".delete", handleDeleteBtnClick);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,63 @@
 var db = require("../models");
 
-// I'm just writing this here as a test
-
 module.exports = function (app) {
   // Get all users for login check
-  app.get("/api/users", function (req, res) {
-    db.User.findAll({}).then(function (getUsers) {
+  app.get("/api/users/:UserName?/:UserPass?", function (req, res) {
+    db.User.findAll({
+      // where: { userName: req.params.userName }
+      // , where: { userPass: req.params.userPass }
+    }).then(function (getUsers) {
       res.json(getUsers);
     });
   });
 
-  // get all bills for a particular user (use on logged in page for particular user)
-  // userID is optional so that all bills for all users can be pulled
-
-  // app.get("/api/bills/:userID?", function (req, res) {
-  //   db.bill.findAll({}).then(function (userBills) {
-  //     res.json(userBills);
-  //   });
-  // });
+  // Create a new User
+  app.post("/api/users/:userName/:Password?", function (req, res) {
+    db.User.create({
+      userName: req.body.userName,
+      Password: req.body.Password
+    }).then(function (newUser) {
+      res.json(newUser);
+    });
+  });
 
   // get all items for a particular user (use on logged in page for particular user)
   // optional user tag to pull data for all users to create graphs
   // optional type tag to pull data for various types from a user to create graphs
 
-  // app.get("/api/items/:userID?/:type?", function(req, res) {
-  //   db.item.findAll({}).then(function(userBills) {
-  //     res.json(userBills);
-  //   });
-  // });
+  app.get("/api/items/:userId?/:type?/:categorey?", function (req, res) {
+    db.Item.findAll({
+      // UserId need to fill automatically from the url... not sure how to do that but the user should not have control of this
+      where: { userId: req.params.userId },
+      // will also need type and category specifiers for the graphical output
+      where: { type: req.params.type },
+      where: { categorey: req.params.categorey }
+    }).then(function (userBills) {
+      res.json(userBills);
+    });
+  });
 
-  // Create a new bill (for a specific user)
-  app.post("/api/users", function(req, res) {
-    db.User.create({
-      password: req.body
-    }).then(function(newUser) {
-      res.json(newUser);
+  // post a new item once logged in as a user
+
+  app.post("/api/items/:userId/:type/:categorey", function (req, res) {
+    db.Item.create({
+      userId: req.params.userId,
+      type: req.params.type,
+      categorey: req.params.categorey
+    }).then(function (postItem) {
+      res.json(postItem);
     });
   });
 
   // Delete a bill (only for logged in user)
-  // app.delete("/api/bills/:id", function(req, res) {
-  //   db.bill.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(refreshPage);
-  //   });
-  // });
+  app.delete("/api/items/:userId/:itemId", function (req, res) {
+    db.Item.destroy({
+      where: { userId: req.params.userId },
+      where: { itemId: req.params.itemId }
+    }).then(function (refreshPage) {
+      res.json(refreshPage);
+    });
+  });
+
+  // end of module.export
 };
