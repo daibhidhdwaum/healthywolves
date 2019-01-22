b01eecdbbaae78aa10a8ef3c29792b7af2847293
Saryn Caister <thegreenmajority@gmail.com>
2019-01-22 14:29:40 -0500

    partial functionality of user list from database on loggedIn still working

---
+++ public/js/loggedIn.js
@@ -0,0 +1,47 @@
+/* eslint-disable linebreak-style */
+$(document).ready(function() {
+  var $existingUsers = $("#existing-users");
+
+  // The API object contains methods for each kind of request we'll make
+  var API = {
+    getUsers: function() {
+      return $.ajax({
+        url: "api/loggedIn",
+        type: "GET"
+      });
+    },
+    deleteExample: function(id) {
+      return $.ajax({
+        url: "api/users/" + id,
+        type: "DELETE"
+      });
+    }
+  };
+
+  // refreshExamples gets new examples from the db and repopulates the list
+  var refreshExamples = function() {
+    API.getUsers().then(function(data) {
+      var $examples = data.map(function(userList) {
+        var $a = $("<a>").text(userList.userName);
+
+        var $li = $("<li>")
+          .attr({
+            class: "list-group-item"
+          })
+          .append($a);
+
+        var $button = $("<button>")
+          .addClass("btn btn-danger float-right delete")
+          .text("ｘ");
+
+        $li.append($button);
+
+        return $li;
+      });
+
+      $existingUsers.empty();
+      $existingUsers.append($examples);
+    });
+  };
+  refreshExamples();
+});
