d8fe5ae3a559cc8a39b583c391106d4bfff445d1
David Downie <daviddownie@gmail.com>
2019-01-23 18:29:32 -0500

    changed button color

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,7 +11,6 @@ module.exports = function (app) {
   });
 
   // Load example page and pass in an example by id
-<<<<<<< HEAD
   app.get("/user/:id", function (req, res) {
     db.user.findOne({ where: { id: req.params.id } }).then(function (UserId) {
       res.render("loggedIn", {
@@ -19,12 +18,6 @@ module.exports = function (app) {
         bill: bill,
         item: item
       });
-=======
-  app.get("/loggedIn/:userid", function(req, res) {
-    db.Item.findAll({}).then(function(
-     currentUser    ) {
-      res.render("loggedIn", currentUser);
->>>>>>> ff588841ee41dcf70379418898a10016cd61c483
     });
   });
 
