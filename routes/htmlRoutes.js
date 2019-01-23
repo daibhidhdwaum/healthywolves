b50555a3a9713d3f032e95ea9bae37e782ad94eb
Saryn Caister <thegreenmajority@gmail.com>
2019-01-22 21:31:03 -0500

    items working on loggedIn

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,27 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     res.render("index");
   });
 
   // Load example page and pass in an example by id
   app.get("/loggedIn/:userid", function(req, res) {
-    db.Item.findAll({}).then(function(
-     currentUser    ) {
-      res.render("loggedIn", currentUser);
+    console.log(req.params.userid);
+    db.Item.findAll({
+      where: {
+        UserUserId: req.params.userid
+      }
+    }).then(function(userData) {
+      console.log(userData);
+      var dataObj = {userData: userData};
+      res.render("loggedIn", dataObj);
     });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
