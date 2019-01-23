1c9e330fa6fd9d28c6fb7fa4796e7496e98280a6
Saryn Caister <thegreenmajority@gmail.com>
2019-01-23 15:05:29 -0500

    edited item model and seed data to reduce input fields down to primary data types and formatted item output to display all data

--- models/seeds.sql
+++ models/seeds.sql
@@ -7,8 +7,8 @@ insert into expenses.users(userName,Password,createdAt,updatedAt) values ('David
 
 -- //items table
 
- insert into expenses.items(Store,DOP,Price,Type,Category,createdAt,updatedAt,UserUserId) values ('Walmart','2019-01-18','1000','Need','Groceries',Now(),Now(),'1');
- insert into expenses.items(Store,DOP,Price,Type,Category,createdAt,updatedAt,UserUserId) values ('BestBuy','2019-01-18','500','Want','Electronics',Now(),Now(),'2');
- insert into expenses.items(Store,DOP,Price,Type,Category,createdAt,updatedAt,UserUserId) values ('Walmart','2019-01-18','300','Want','Accessories',Now(),Now(),'3');
- insert into expenses.items(Store,DOP,Price,Type,Category,createdAt,updatedAt,UserUserId) values ('Costco','2019-01-18','10','Want','Gardening',Now(),Now(),'2');
- insert into expenses.items(Store,DOP,Price,Type,Category,createdAt,updatedAt,UserUserId) values ('Walmart','2019-01-18','90','Need','Groceries',Now(),Now(),'1');
+ insert into expenses.items(Price,Typeof,Category,createdAt,updatedAt,UserUserId) values ('1000','Need','Groceries',Now(),Now(),'1');
+ insert into expenses.items(Price,Typeof,Category,createdAt,updatedAt,UserUserId) values ('500','Want','Electronics',Now(),Now(),'2');
+ insert into expenses.items(Price,Typeof,Category,createdAt,updatedAt,UserUserId) values ('300','Want','Accessories',Now(),Now(),'3');
+ insert into expenses.items(Price,Typeof,Category,createdAt,updatedAt,UserUserId) values ('10','Want','Gardening',Now(),Now(),'2');
+ insert into expenses.items(Price,Typeof,Category,createdAt,updatedAt,UserUserId) values ('90','Need','Groceries',Now(),Now(),'1');
